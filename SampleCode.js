1b37b9a658b4cb12907592811df3d9d2b2509d4a
Ameer Hamza <114015978+ameerhamza10@users.noreply.github.com>
2023-09-30 16:08:05 +0500

    change variable name

--- SampleCode.js
+++ SampleCode.js
@@ -1,11 +1,11 @@
-let num1 = parseFloat(prompt("Enter the first number: "));
-let num2 = parseFloat(prompt("Enter the second number: "));
+let var1 = parseFloat(prompt("Enter the first number: "));
+let var2 = parseFloat(prompt("Enter the second number: ")); 
 
+let sumResult = var1 + var2;
 
-let sumResult = num1 + num2;
-
-alert(`The sum of ${num1} and ${num2} is ${sumResult}`);
+alert(`The sum of ${var1} and ${var2} is ${sumResult}`);
 
 let string= "this is changes and i am on branch CS-311-Web-Programming"
 
 console.log(string);
+
