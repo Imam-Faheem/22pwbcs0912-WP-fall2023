f754f7d75e3bd9b05b5864aeb272f951c4438555
Imam Faheem <132200539+Imam-Faheem@users.noreply.github.com>
2023-09-30 03:18:20 -0700

    SampleCode.js
    code in javascript for addition of 2 number

---
+++ SampleCode.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt("Enter the first number: "));
+let num2 = parseFloat(prompt("Enter the second number: "));
+
+
+let sumResult = num1 + num2;
+
+alert(`The sum of ${num1} and ${num2} is ${sumResult}`);
