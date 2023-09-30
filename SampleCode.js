8c9a9bce3790ea5adbd32e08047f611fd1d96873
Imam Faheem <imamhusain941@gmail.com>
2023-09-30 03:52:49 -0700

    string is added and consoled

--- SampleCode.js
+++ SampleCode.js
@@ -5,3 +5,7 @@ let num2 = parseFloat(prompt("Enter the second number: "));
 let sumResult = num1 + num2;
 
 alert(`The sum of ${num1} and ${num2} is ${sumResult}`);
+
+let string= "this is changes and i am on branch CS-311-Web-Programming"
+
+console.log(string);
